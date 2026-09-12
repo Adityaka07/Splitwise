9c53ecf7261b4e76008b6bcac0c72266cf1951df
Aditya <adityakakade1347@gmail.com>
2026-09-12 14:27:18 +0530

    Connected the expense and group class and tried everything in the main-milestone three completed
    In the group class, we added the expense array and we added an add expense function which link to the expense group and added an expense object to the expense area in the group, and then we tested everything in the main.
    Everything works fine and is in good shape

--- SW/splitwise-backend/src/main/java/com/splitwise/Main.java
+++ SW/splitwise-backend/src/main/java/com/splitwise/Main.java
@@ -1,13 +1,16 @@
 package com.splitwise;
 import com.splitwise.model.Group;
 import com.splitwise.model.User;
+import java.util.ArrayList;
+import java.util.List;
+import com.splitwise.model.Expense;
 
 public class Main {
     public static void main(String[] args) {
         User U1 = new User("Aditya");
         User U2 = new User("u2");
         User U3 = new User("u3");
-        Group g3 = new Group("pliers");
+        Group g3 = new Group("Trip");
         System.out.println(U1.toString());
         System.out.println(U2.toString());
         System.out.println(U3.toString());
@@ -15,6 +18,13 @@ public class Main {
         g3.addMember(U1);
         g3.addMember(U2);
         g3.addMember(U1);                               //testing the ad-members function in the group class works as I try to input the user one twice
+        List<User> takers = new ArrayList<>();
+        takers.add(U1);
+        takers.add(U2);
+        
+        Expense E1 = new Expense(U1,"dinner", "1200", takers);
+        g3.addExpense(E1);
         System.out.println(g3);
+
     }
 }
--- SW/splitwise-backend/src/main/java/com/splitwise/model/Group.java
+++ SW/splitwise-backend/src/main/java/com/splitwise/model/Group.java
@@ -6,12 +6,14 @@ public class Group {
     private String groupID;
     private String groupName;
     private List<User> users;                           //this is the place which is a list in which we will store all the users object, the data type of this list is the user object, so we can store the whole object in this, not only the user IDs or stuff
+    private List<Expense> expenses;
 
     public Group(String groupName)
     {
         this.groupID = UUID.randomUUID().toString();
         this.groupName = groupName;
         this.users = new ArrayList<>();
+        this.expenses = new ArrayList<>();
     }
 
     public void addMember(User newUser)
@@ -26,12 +28,28 @@ public class Group {
         }
         this.users.add(newUser);
     }
+    public void addExpense(Expense newExpense)
+    {
+        for(Expense E : expenses)
+        {
+            if(E.getTransactionID().equals(newExpense.getTransactionID()))
+            {
+                System.out.println("This transaction has already been logged");
+                return;
+            }
+        }
+        this.expenses.add(newExpense);
+    }
     public List<User> getUsers()
     {
         return this.users;
     }
+    public List<Expense> getExpenses()
+    {
+        return this.expenses;
+    }
     @Override public String toString()
     {
-        return "Group{groupID='"+this.groupID+"',groupName='"+this.groupName+"',Users='"+this.users+"'}";       //in this two string function, when we write users equal to this.users, Java automatically loops through all the users in the list and prints it automatically. We don't have to do anything.
+        return "Group{groupID='"+this.groupID+"',groupName='"+this.groupName+"',Users='"+this.users+"' ,Expenses = '"+this.expenses+"'}";       //in this two string function, when we write users equal to this.users, Java automatically loops through all the users in the list and prints it automatically. We don't have to do anything.
     }
 }
