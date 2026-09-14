32dfadaac45018db99359428ea1062751f9dc7f5
Aditya <adityakakade1347@gmail.com>
2026-09-14 21:15:28 +0530

    made the Split abstract class and Equal Split implementation
    removed the equal split class which was earlier with an abstract class and its implementation

---
+++ SW/splitwise-backend/src/main/java/com/splitwise/split/Split.java
@@ -0,0 +1,27 @@
+package com.splitwise.split;
+
+import java.math.BigDecimal;
+
+import com.splitwise.model.User;
+
+public abstract class Split {
+    private User user;
+    private BigDecimal amount;
+
+    public Split(User user,BigDecimal amount)
+    {
+        this.user = user;
+        this.amount = amount;
+    }
+    public abstract boolean validate();
+
+public User getUser()
+{
+    return this.user;
+}
+public BigDecimal getAmount()
+{
+    return this.amount;
+}
+
+}
